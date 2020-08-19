0dcecacf1dc743cfd08c06cd6c589e6dd3a541a5
Liuhuan247 <Liuhuan247@users.noreply.github.com>
2020-08-19 15:20:37 +0000

    Deploying to gh-pages from  @ 9e22b0e99df30bd08216492ccceb3e8730305883 🚀

---
+++ precache-manifest.ca091e1098ec63f16c9491e1f0cbc0b6.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0f23ccbe15950760534532759ea31fc",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.5d756215774314de0ab8.bundle.js"
+  },
+  {
+    "url": "runtime~main.5d756215774314de0ab8.bundle.js"
+  },
+  {
+    "url": "vendors~main.5d756215774314de0ab8.bundle.js"
+  },
+  {
+    "url": "vendors~main.5d756215774314de0ab8.bundle.js.LICENSE.txt"
+  }
+]);
