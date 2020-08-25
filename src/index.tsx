54f88eae0a72c5851bf2a7541527df269ff60ddf
刘焕 <liuhuan247_2014@163.com>
2020-08-25 21:58:11 +0800

    feat: badge radio components

--- src/index.tsx
+++ src/index.tsx
@@ -2,4 +2,6 @@ export * from "./components/button";
 export * from "./components/highlight";
 export * from "./components/shared/global";
 export * from "./components/shared/styles";
-export * from "./components/shared/animation";
+export * from "./components/icon";
+export * from "./components/radio";
+export * from "./components/badge";
