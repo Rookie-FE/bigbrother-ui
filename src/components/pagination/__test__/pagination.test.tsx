f638f035c8315b37b37b0a18912c5fb712d9e77e
刘焕 <liuhuan247_2014@163.com>
2020-09-05 21:55:24 +0800

    feat: pagination

---
+++ src/components/pagination/__test__/pagination.test.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { render, fireEvent, cleanup } from "@testing-library/react";
+import { Pagination } from "../index";
+import { color, typography } from "../../shared/styles";
+
+describe("test Pagination component", () => {
+  it('render correctly', () => {
+    const wrapper = render(
+      <Pagination />
+    );
+    expect(wrapper).toMatchSnapshot();
+  });
+})
