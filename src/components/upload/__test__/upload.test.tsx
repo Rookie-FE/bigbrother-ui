4528db42de067931ea6d1c9dcd8d085204de7982
刘焕 <liuhuan247_2014@163.com>
2020-09-05 21:43:56 +0800

    feat: upload message etc.

---
+++ src/components/upload/__test__/upload.test.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { render, fireEvent, cleanup } from "@testing-library/react";
+import { Upload } from "../index";
+import { color, typography } from "../../shared/styles";
+
+describe("test Upload component", () => {
+  it('render correctly', () => {
+    const wrapper = render(
+      <Upload />
+    );
+    expect(wrapper).toMatchSnapshot();
+  });
+})
